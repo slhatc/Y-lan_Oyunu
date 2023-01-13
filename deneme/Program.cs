5181736b4c3903418e59c4531f62f95aec33b247
slhatc50 <slhatc50@outlook.com>
2023-01-13 15:32:03 +0300

    Add project files.

---
+++ deneme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? minvalue = null;
+            int deger = -5;
+            if (deger< minvalue)
+            {
+                Console.WriteLine("sonuc:");
+            }
+            Console.WriteLine("Değer:", minvalue);
+            Console.ReadLine();
+        }
+    }
+}
